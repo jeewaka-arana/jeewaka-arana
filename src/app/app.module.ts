e60c4e448b30892ef08248a7a97f785beceaf21c
piyugirl <piyugirl02@yahoo.com>
2019-09-16 17:51:38 -0800

    e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,17 +50,7 @@ import { DocLoginComponent } from './userlogin/doc-login/doc-login.component';
 import { PatLoginComponent } from './userlogin/pat-login/pat-login.component';
 import { DropzoneDirective } from './doctor/dropzone.directive';
 import { UploaderComponent } from './doctor/uploader/uploader.component';
-import { UploadTaskComponent } from './doctor/upload-task/upload-task.component';
-import { CommentComponent } from './doctor/commentsection/comment/comment.component';
-
-
-
-
-
-
-
-
-
+import { UploadTaskComponent } from './doctor/upload-task/upload-task.component'
 
 
 
@@ -99,7 +89,7 @@ import { CommentComponent } from './doctor/commentsection/comment/comment.compon
         DropzoneDirective,
         UploaderComponent,
         UploadTaskComponent,
-        CommentComponent,
+      
      
        
        
