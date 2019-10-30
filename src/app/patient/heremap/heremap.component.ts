b615001e4e6b0bb061deb0d84f894e1e8ae626c8
amayaherath <dinithiamaya@gmail.com>
2019-10-30 13:44:44 +0530

    ewe

--- src/app/patient/heremap/heremap.component.ts
+++ src/app/patient/heremap/heremap.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , ViewChild, ElementRef, Input,} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { HereService } from "app/core/here.service";
 
 declare var H: any;
@@ -10,39 +10,81 @@ declare var H: any;
 })
 export class HeremapComponent implements OnInit {
 
+  @ViewChild("map")
+    public mapElement: ElementRef;
+
+    @Input()
+    public appId: any;
+
+    @Input()
+    public appCode: any;
+
+    @Input()
+    public lat: any;
+
+    @Input()
+    public lng: any;
+
+    @Input()
+    public width: any;
+
+    @Input()
+    public height: any;
+
+    private platform: any;
+    private map: any;
+
+    public constructor() { }
+
  
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    public ngOnInit(){
+      this.platform = new H.service.Platform({
+        "app_id": this.appId,
+        "app_code": this.appCode
+    });
+    // throw new Error("Method not implemented.");
   }
+
+  public ngAfterViewInit() {
+    let defaultLayers = this.platform.createDefaultLayers();
+    this.map = new H.Map(
+        this.mapElement.nativeElement,
+        defaultLayers.normal.map,
+        {
+            zoom: 10,
+            center: { lat: this.lat, lng: this.lng }
+        }
+    );
+}
   
 
-  public query: string;
-    public position: string;
-    public locations: Array<any>;
+//   public query: string;
+//     public position: string;
+//     public locations: Array<any>;
 
-    public constructor(private here: HereService) {
-        this.query = "Tracy, CA";
-        this.position = "37.7397,-121.4252";
-    }
+//     public constructor(private here: HereService) {
+//         this.query = "Tracy, CA";
+//         this.position = "37.7397,-121.4252";
+//     }
 
-    public getAddress() {
-      if(this.query != "") {
-          this.here.getAddress(this.query).then(result => {
-              this.locations = <Array<any>>result;
-          }, error => {
-              console.error(error);
-          });
-      }
-  }
+//     public getAddress() {
+//       if(this.query != "") {
+//           this.here.getAddress(this.query).then(result => {
+//               this.locations = <Array<any>>result;
+//           }, error => {
+//               console.error(error);
+//           });
+//       }
+//   }
 
-  public getAddressFromLatLng() {
-    if(this.position != "") {
-        this.here.getAddressFromLatLng(this.position).then(result => {
-            this.locations = <Array<any>>result;
-        }, error => {
-            console.error(error);
-        });
-    }
-}
+//   public getAddressFromLatLng() {
+//     if(this.position != "") {
+//         this.here.getAddressFromLatLng(this.position).then(result => {
+//             this.locations = <Array<any>>result;
+//         }, error => {
+//             console.error(error);
+//         });
+//     }
+// }
 
 }
